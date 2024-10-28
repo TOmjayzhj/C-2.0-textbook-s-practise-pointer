1410e6e036a75270c4e2f37f5dcb834522335e5a
TOmjayzhj <942934710@qq.com>
2024-10-28 23:26:33 +0800

    Add files via upload

---
+++ 水仙花刷题版.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<math.h>
+int p(int a,int b);
+int main(void)
+{
+	int mark,num,sum,t;
+	int i,j,n;
+	scanf("%d",&n);
+	mark=1;
+	for(i=0;i<n;i++){
+		mark*=10;
+	} 
+	for(i=mark/10;i<mark;i++){
+		sum=0;
+		//int t=i;
+		for(t=i;t>0;t/=10){
+			j=t%10;
+			//t/=10;
+			sum+=p(j,n);
+		}
+		if(sum==i)printf("%d\n",i);
+	}
+	return 0;
+}
+int p(int a,int b)
+{
+	int i;
+	int k=1;
+	for(i=1;i<=b;i++){
+		k=k*a;
+	}
+	return k;
+}
+
