0d44c7c95a524ed13ebdfa48fe3fa86a015d9f20
TOmjayzhj <942934710@qq.com>
2024-10-31 22:36:23 +0800

    Update 数组插入和删除.c

--- 数组插入.c
+++ 数组插入.c
@@ -1,4 +1,4 @@
-#include<stdio.h>
+#include<stdio.h>                                            
 int main(void)
 {
 	int i,n,j;
@@ -18,6 +18,32 @@ int main(void)
 	}
 	a[count]=n;
 	for(i=0;i<6;i++){
+		printf("%d ",a[i]);
+	}
+	return 0;
+}
+++++++++++++++++++++++++++++++++++++++++
+数组删除
+#include<stdio.h>
+int main(void)
+{
+	int i,n,j;
+	int count=0;
+	int a[10]={1,4,7,8,9};
+	printf("1,4,7,8,9\n");
+	scanf("%d",&n);
+	for(i=0;i<5;i++){
+		if(n<=a[i]){	
+			break;
+		}
+		count++;
+	}
+	a[count]=a[count+1];
+	for(j=count;j<=4;j++){
+		a[j]=a[j+1];
+	}
+	
+	for(i=0;i<4;i++){
 		printf("%d ",a[i]);
 	}
 	return 0;
